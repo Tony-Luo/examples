1016a6a377ce649223faaceb32240c7b42fcda96
TonyLuo <tonyjobgithub@gmail.com>
2016-06-18 20:56:33 -0700

    Update home page

--- js/app.js
+++ js/app.js
@@ -23,42 +23,56 @@ angular.module('app', ['ionic'])
   });
 })
 
-.controller('AppCtrl', function($scope, $ionicModal) {
+.controller('HomeCtrl', function($scope, $ionicModal) {
   $scope.countries = {
-    "us": {
-      img: 'img/usp1.jpg',
-      alt: 'US plane F4U Corsair',
-      model: 'F4U Corsair',
-      manufacturer: 'Chance Vought',
-      totalBuiltNumber: '12,571',
+    "allies": {
+      "us": {
+        img: 'img/usp1.jpg',
+        modalImg: 'img/us.jpg',
+        alt: 'US plane F4U Corsair',
+        motto: 'Aim High ... Fly-Fight-Win',
+        model: 'F4U Corsair',
+        manufacturer: 'Chance Vought',
+        totalBuiltNumber: '12,571',
+      },
+      "uk": {
+        img: 'img/ukp1.jpg',
+        modalImg: 'img/uk.jpg',
+        alt: 'UK plane Spitfire',
+        motto: 'Through Adversity to the Stars',
+        model: 'Spitfire',
+        manufacturer: 'Supermarine',
+        totalBuiltNumber: '20,351',
+      },
+      "ussr": {
+        img: 'img/ussrp1.jpg',
+        modalImg: 'img/ussr.jpg',
+        alt: 'USSR plane La-5',
+        motto: 'For Mother Russia',
+        model: 'La-5',
+        manufacturer: 'Lavochkin',
+        totalBuiltNumber: '9,920',
+      }
     },
-    "uk": {
-      img: 'img/ukp1.jpg',
-      alt: 'UK plane Spitfire',
-      model: 'Spitfire',
-      manufacturer: 'Supermarine',
-      totalBuiltNumber: '20,351',
-    },
-    "ussr": {
-      img: 'img/ussrp1.jpg',
-      alt: 'USSR plane La-5',
-      model: 'La-5',
-      manufacturer: 'Lavochkin',
-      totalBuiltNumber: '9,920',
-    },
-    "germany": {
-      img: 'img/germanyp1.jpg',
-      alt: 'Germany plane Bf 109',
-      model: 'Bf 109',
-      manufacturer: 'Chance Vought',
-      totalBuiltNumber: '33,984',
-    },
-    "japan": {
-      img: 'img/japanp1.jpg',
-      alt: 'Japan plane A6M Zero',
-      model: 'A6M Zero',
-      manufacturer: 'Mitsubishi',
-      totalBuiltNumber: '10,939',
+    "axis": {
+      "germany": {
+        img: 'img/germanyp1.jpg',
+        modalImg: 'img/germany.jpg',
+        alt: 'Germany plane Bf 109',
+        motto: 'Sieg Heil',
+        model: 'Bf 109',
+        manufacturer: 'Chance Vought',
+        totalBuiltNumber: '33,984',
+      },
+      "japan": {
+        img: 'img/japanp1.jpg',
+        modalImg: 'img/japan.jpg',
+        alt: 'Japan plane A6M Zero',
+        motto: 'Long live the Emperor',
+        model: 'A6M Zero',
+        manufacturer: 'Mitsubishi',
+        totalBuiltNumber: '10,939',
+      }
     }
   };
 
@@ -71,13 +85,48 @@ angular.module('app', ['ionic'])
   });
 
   $scope.openModal = function(country) {
-    $scope.model = $scope.countries[country].model;
-    $scope.manufacturer = $scope.countries[country].manufacturer;
-    $scope.totalBuiltNumber = $scope.countries[country].totalBuiltNumber;
+    switch(country) {
+      case 'germany':
+      case 'japan':
+        $scope.motto = $scope.countries.axis[country].motto;
+        $scope.model = $scope.countries.axis[country].model;
+        $scope.manufacturer = $scope.countries.axis[country].manufacturer;
+        $scope.totalBuiltNumber = $scope.countries.axis[country].totalBuiltNumber;
+        $scope.modalImg = $scope.countries.axis[country].modalImg;
+        $scope.alt = $scope.countries.axis[country].alt;
+        break;
+      default:
+        $scope.motto = $scope.countries.allies[country].motto;
+        $scope.model = $scope.countries.allies[country].model;
+        $scope.manufacturer = $scope.countries.allies[country].manufacturer;
+        $scope.totalBuiltNumber = $scope.countries.allies[country].totalBuiltNumber;
+        $scope.modalImg = $scope.countries.allies[country].modalImg;
+        $scope.alt = $scope.countries.allies[country].alt;
+        break;
+    }
     $scope.modal.show();
   };
   
   $scope.closeModal = function() {
     $scope.modal.hide();
   };
-});
+})
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html',
+      controller: 'HomeCtrl'
+    })
+    .state('allies', {
+      url: '/allies',
+      templateUrl: 'allies.html'
+    })
+    .state('axis', {
+      url: '/axis',
+      templateUrl: 'axis.html'
+    });
+  
+  $urlRouterProvider.otherwise('/');
+}]);
